629b345d92a18c856e0297a8c46f43e953be0202
JEONG MINSOO <minsu3463@gmail.com>
2023-07-01 21:53:37 +0900

    Initial commit

---
+++ src/test/java/com/spring_study/spring_study/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_study.spring_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
